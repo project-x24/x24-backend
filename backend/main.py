2bfe66db69c0275542b8120f49eef9838eed836a
Ayush Shankarpurkar <ayushshankarpurkar11@gmail.com>
2024-03-09 00:39:36 +0530

    cleaned the code

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
-
-class User(BaseModel):
-    name: str
-    age: int
-    email: str
 
 @app.get("/")
 async def root():
